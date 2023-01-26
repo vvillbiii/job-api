5c3782caba46c8b591475a3ef0dba8c8d2393cee
vvillbii <will.bowles90@gmail.com>
2023-01-25 22:43:52 -0500

    add nodemon

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ dotenv.config({ path: "./config/config.env" });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+  console.log(`Server is listening on ${PORT} in ${process.env.NODE_ENV}`);
 });
