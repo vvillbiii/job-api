c0411066a575da9d4b4a830d37b6a7b0cec2a4da
vvillbii <will.bowles90@gmail.com>
2023-01-25 22:26:08 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+// Setting up config.env
+dotenv.config({ path: "./config/config.env" });
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
