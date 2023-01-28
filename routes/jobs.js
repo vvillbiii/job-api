3c25fcd957b401402d4a94e9e935cc91ce4b7996
vvillbii <will.bowles90@gmail.com>
2023-01-27 22:32:21 -0500

    add get jobs in radius route

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 //importing jobs controllers
-const { getJobs, newJob } = require("../controllers/jobsController");
+const {
+  getJobs,
+  newJob,
+  getJobsInRadius,
+} = require("../controllers/jobsController");
 
 router.route("/jobs").get(getJobs);
+router.route("/jobs/:zipcode/:distnace").get(getJobsInRadius);
 router.route("/jobs/new").post(newJob);
 
 module.exports = router;
