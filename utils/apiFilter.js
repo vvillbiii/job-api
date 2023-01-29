5d85475b978b31334373e98fd144544adfd8da48
vvillbii <will.bowles90@gmail.com>
2023-01-29 17:15:59 -0500

    add sort function to apifilter.js

--- utils/apiFilter.js
+++ utils/apiFilter.js
@@ -7,6 +7,10 @@ class APIFilter {
   filter() {
     const queryCopy = { ...this.queryStr };
 
+    //removing fields from query
+    const removeFields = ["sort"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
     // advance filter using: lt, lte, gt, gte
     let queryStr = JSON.stringify(queryCopy);
     queryStr = queryStr.replace(
@@ -14,6 +18,17 @@ class APIFilter {
       (match) => `$${match}`
     );
     this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryStr.sort) {
+      const sortBy = this.queryStr.sort.split(",").join();
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-postingDate");
+    }
+
     return this;
   }
 }
