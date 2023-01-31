1f35a35a531ca01a1902f85d5f6298bce85740fa
vvillbii <will.bowles90@gmail.com>
2023-01-30 22:25:27 -0500

    handle duplicate key error in mongoose

--- middleware/errors.js
+++ middleware/errors.js
@@ -29,6 +29,12 @@ module.exports = (err, req, res, next) => {
       error = new ErrorHandler(message, 404);
     }
 
+    //handle mongoose duplicate key error
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered.`;
+      error = new ErrorHandler(message, 404);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error.",
