049e3ada33c99b95048539f2dc8ddc4bf48a1238
vvillbii <will.bowles90@gmail.com>
2023-02-08 06:27:30 -0500

    add jobs published route to user routes

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ const {
   updateUser,
   deleteUser,
   getAppliedJobs,
+  getPublishedJobs,
 } = require("../controllers/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
@@ -14,6 +15,14 @@ router.route("/me").get(isAuthenticatedUser, getUserProfile);
 router
   .route("/jobs/applied")
   .get(isAuthenticatedUser, authorizeRoles("user"), getAppliedJobs);
+
+router
+  .route("/jobs/published")
+  .get(
+    isAuthenticatedUser,
+    authorizeRoles("employer", "admin"),
+    getPublishedJobs
+  );
 
 router.route("/me/update").put(isAuthenticatedUser, updateUser);
 
